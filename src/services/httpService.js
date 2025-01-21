c2f898152d237ecd810da66ab996eb8a27f6afb2
Aqib Asif <mirza_aqib@live.com>
2025-01-21 12:11:02 +0500

    fix: 🔧 test commit

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,10 +1,12 @@
-import axios from 'axios';
-import logger from './logService';
-import { toast } from 'react-toastify';
+import axios from "axios";
+import logger from "./logService";
+import { toast } from "react-toastify";
 
 // axios.defaults.baseURL = 'http://localhost:3800/api';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+console.log("SNV", process.env.REACT_APP_API_URL);
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
@@ -14,13 +16,13 @@ axios.interceptors.response.use(null, (error) => {
 
   if (!expectedError) {
     logger.log(error);
-    toast.error('An unexpected error occurrred.');
+    toast.error("An unexpected error occurrred.");
   }
   return Promise.reject(error);
 });
 
 export function setJwt(jwt) {
-  axios.defaults.headers.common['x-auth-token'] = jwt;
+  axios.defaults.headers.common["x-auth-token"] = jwt;
 }
 
 var http = {
