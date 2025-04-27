a3f63f64f3a97e5668f639a70db0ec423b9e06f6
Aqib Asif <mirza_aqib@live.com>
2025-04-27 16:27:07 +0500

    refactor: 🌿 code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import RouteNavigation from "./Routes/RouteNavigation";
 import { ToastContainer } from "react-toastify";
 import { ThemeProvider } from "@mui/material";
+import createAppTheme from "./theme";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import "./ToastStyles.css";
-import createAppTheme from "./theme";
 
 const App = () => {
-  // const googleClientId = process.env.REACT_APP_GOOGLE_OAUTH_ID ?? "";
   const theme = createAppTheme();
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // const fetchData = async () => {
-  //   setLoading(true);
-  //   await fetchCategories();
-  //   setLoading(false);
-  // };
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <GoogleOAuthProvider clientId={googleClientId}> */}
-      {/* {loading ? <></> : <RouteNavigation />} */}
       <RouteNavigation />
       <ToastContainer hideProgressBar autoClose={2500} />
-      {/* </GoogleOAuthProvider> */}
     </ThemeProvider>
   );
 };
