977f5216f252e42b2a0c0e9291e13b3c5164d2e2
Aqib Asif <mirza_aqib@live.com>
2025-05-11 15:41:07 +0500

    fix: 🐞 unnecessary imports removed

--- src/Components/Common/CustomMultiSelect.tsx
+++ src/Components/Common/CustomMultiSelect.tsx
@@ -1,13 +1,8 @@
-import { Box } from '@mui/material';
-import * as React from 'react';
+import { Box } from "@mui/material";
 
 const CustomMultiSelect = () => {
-  return ( 
-    <Box>
-
-    </Box>
-   );
-}
+  return <Box></Box>;
+};
 // TODO: move the FE to NextJs, same files same logics for now, later can shift the logic to Next15 as well
- 
+
 export default CustomMultiSelect;
